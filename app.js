418787c4432375f954698ce5b24b4ecb5c17e2b2
lhumeau <luishumeau90@gmail.com>
2022-12-07 22:20:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,84 +1,47 @@
-const {
-    Client,
-    Location,
-    List,
-    Buttons,
-    LocalAuth,
-    Chat,
-    Message,
-} = require("./index");
-
-const qrcode = require("qrcode-terminal");
-const accionar = require("./accionar");
-const posdata = require("./postData");
-var qrcodeVar = ''
+/* eslint-disable linebreak-style */
+const { Client, LocalAuth } = require('./index'); // \r\n importamos la libreria
+const qrcode = require('qrcode-terminal'); // importamos la libreria // \r\n
+const accionar = require('./accionar'); // importamos la libreria // \r\n
+const posdata = require('./postData'); // importamos la libreria // \r\n
 const client = new Client({
     authStrategy: new LocalAuth(),
-          puppeteer: { headless: true,
-                       executablePath: '/bin/chromium-browser',
-                       args: ['--no-sandbox'],
-                      
-                     
-                     },
-          // activa el chromiun
-    /*    puppeteer: {
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        headless: false 
-       
-            }    */
-
-    });
-
-
-        
-        
-
-const chat = new Chat({});
-const message = new Message({});
-
-
-
-client.on("loading_screen", (percent, message) => {
-    console.log("LOADING SCREEN", percent, message);
+    puppeteer: { headless: true,
+                 executablePath: '/bin/chromium-browser',
+                 args: ['--no-sandbox'],
+               },
 });
 
-client.on("qr", (qr) => {
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
+});
+
+client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
-    console.log('QR RECEIVED',`'${qr}'`);
-
+    console.log('QR RECEIVED', `'${qr}'`);
 });
 
-client.on("authenticated", () => {
-    console.log("AUTHENTICATED");
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
 });
 
-client.on("auth_failure", (msg) => {
+client.on('auth_failure', (msg) => {
     // Fired if session restore was unsuccessful
-    console.error("AUTHENTICATION FAILURE", msg);
+    console.error('AUTHENTICATION FAILURE', msg);
 });
 
-client.on("ready", () => {
-    console.log("Client is ready!");
+client.on('ready', () => {
+    console.log('Client is ready!');
 });
 
-//Variables
-
-var ficha = `Favor de indicar la Ficha Con Guiones.
+var askfichaMessage = `Favor de indicar la Ficha Con Guiones.
              Como por ejemplo: 
              *VL-M45*`;
-var counter = `Favor de indicar 
+var askCounterOdometer = `Favor de indicar 
                *Horometro/Odometro.*`;
-var dev = "Opción en Desarrollo";
-var debesElegir = "Debe elegir una de las opciones";
-const invokeKey = "!b";
-var option1R = "1R";
-var option2R = "2R";
-var option3R = "3R";
 var regficha;
 var regOdometro;
-var regDataConfirm = "🚀Procesando los datos...";
-var startConversation = "Hola";
-var ficha2 = `
+var messageRegDataConfirm = '🚀Procesando los datos...';
+var fixValidatorFicha = `
             *👀Lea atentamente lo que se le pide en cada opción y responda adecuadamente.👀*
 
             ❌Favor verificar su ficha nuevamente, asegurese de que su ficha cumpla con lo siguiente:  
@@ -88,30 +51,43 @@ var ficha2 = `
             - Que contenga numeros y letras.
             - Que no tenga espacios delante ni detras.
             - No enviar mensajes en blanco.
-            - No debe colocar ni enviar emojis.❌`
+            - No debe colocar ni enviar emojis.❌`;
 
-var urlPowerautomate = "https://prod-93.westus.logic.azure.com:443/workflows/d9f08a8dc01d48fe994631b08ea80db6/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r9iB23oBpcLglT1lo6lZCgHnl_OX2nqfHH6tyTr2Cu8";
- 
-var dataJson;
-var htmlResponseCode;
-var statusInsert = "Success";
+var fixValidatorOdometer = `
+            *👀Lea atentamente lo que se le pide en cada opción y responda adecuadamente.👀*
+
+            ❌Favor verificar su Odometro nuevamente, asegurese de que su Odometro cumpla con lo siguiente:  
+            - Solo debe contener números.
+            - Los unidad de miles puede separarse con coma.
+            - El separador de decimal es el "." (Punto).
+            - No enviar mensajes en blanco.
+            - Que no tenga espacios delante ni detras.
+            - No debe colocar ni enviar emojis.
+            - No especifique la unidad si es Kilometraje o Milla.
+            ❌`;
+
+
+var urlPowerautomate =
+  'https://prod-93.westus.logic.azure.com:443/workflows/d9f08a8dc01d48fe994631b08ea80db6/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r9iB23oBpcLglT1lo6lZCgHnl_OX2nqfHH6tyTr2Cu8';
 
 // On create message
 
 client.initialize();
 
-client.on("message_create", async (msg) => {
-    var mensaje = await msg;
-    var numberFrom = mensaje.from
-    numberFrom = numberFrom.replace(/\D/g, "");
-    var chatId = await accionar.sanitizeNumber(mensaje);
-    var chatIdInfo = await client.getChatById(chatId);
-    var lastAnswerBot = await accionar.getLastMessageFromBot(chatIdInfo);
-    var lastAnswerUser = await accionar.getLastMessageFromUser(chatIdInfo);
-   
+client.on('message_create', async (msg) => {
+    var messageIncoming = await msg;
+    var numberFrom = messageIncoming.from;
+    var lastAnswerBot = [];
+    var lastAnswerUser = [];
+    numberFrom = numberFrom.replace(/\D/g, '');
+    var storeChatIdValue = await accionar.sanitizeNumber(messageIncoming);
+    var chatIdInfo = await client.getChatById(storeChatIdValue);
+    lastAnswerBot = await accionar.getLastMessageFromBot(chatIdInfo);
+    lastAnswerUser = await accionar.getLastMessageFromUser(chatIdInfo);
+    //chatIdInfo.clearMessages();
 
-    var menu = `
-👷‍♀️👷‍♂️ Hola *${mensaje._data.notifyName}*,
+    var menuOptions = `
+👷‍♀️👷‍♂️ Hola *${messageIncoming._data.notifyName}*,
 *Lea atentamente lo que se le pide en cada opción y responda adecuadamente.*
 
 Indique la opción a realizar:
@@ -120,180 +96,146 @@ Indique la opción a realizar:
  2️⃣ - En Desarrollo.🔧...
  3️⃣ - En Desarrollo.🔧...
 
-  `
-    ;
-    console.log("respuesta usuario", lastAnswerUser.length);
-    console.log("respuesta Bot", lastAnswerBot.length);
-    
-    if (mensaje.fromMe) // Determinar de remitente de mensaje.
-    {
-        
-        if (lastAnswerBot.length === 0) 
-        {          
-            return client.sendMessage(msg.from, menu);
+  `;
+    console.log('respuesta usuario', lastAnswerUser.length);
+    console.log('respuesta Bot', lastAnswerBot.length);
+
+    if (messageIncoming.fromMe) {
+        // Determinar de remitente de mensaje.
+        if (
+            lastAnswerBot.length === 0
+        ) {
+            return client.sendMessage(msg.from, menuOptions);
         }
 
-        if (lastAnswerBot[0].body == menu) {
-            return console.log("Esperando respuesta bot desde el bot 1");
-        } 
-    } 
-
-    else 
-    
-    {
-      
-       if (lastAnswerUser.length === 1 && lastAnswerBot.length === 0  ) 
-        {
-            
-            return client.sendMessage(msg.from, menu)
-            
+        if (
+            lastAnswerBot[0].body == menuOptions
+        ) {
+            return console.log('El bot envio el menu el usuario debe responder para su captura');
         }
-        if (lastAnswerUser.length === 2 && lastAnswerBot.length === 0  ) 
-        {
-            
-            return client.sendMessage(msg.from, menu)
-            
+        if (
+            lastAnswerBot[0].body == fixValidatorFicha || lastAnswerBot[0].body == askfichaMessage
+        ) {
+            return console.log('El bot envio la ficha el usuario debe responder para su captura');
         }
-        if (lastAnswerUser.length === 1 && lastAnswerBot.length === 1  ) {
-           
-            console.log("Usuario Respondio en valor 1/1");
-             
-                   
-        } 
-        if (lastAnswerUser.length === 0 && lastAnswerBot.length === 0  ) {
-           
-            console.log("Usuario Respondio en Valor 0/1");
-            return client.sendMessage(msg.from, menu);
-
-             
-                   
+        if (
+            lastAnswerBot[0].body == fixValidatorOdometer || lastAnswerBot[0].body == askCounterOdometer
+        ) {
+            return console.log('El bot envio la Horometro/Odometro el usuario debe responder para su captura');
         }
-        
-
-        if (lastAnswerUser.length === 0 && lastAnswerBot[0].body == ficha || lastAnswerUser.length === 0 && lastAnswerBot[0].body == ficha2 ) 
-        {
-            console.log(mensaje, 'Valoes de mensaje')
-    
-            
-            lastAnswerUser[0] = mensaje._data.body;
-            lastAnswerBot[0].body = lastAnswerBot[1].body
-            console.log(lastAnswerBot[0].body, "lastAnswerBot[0].body 0/2")
-                     
-          
-        } 
-        if (lastAnswerUser.length === 0 && lastAnswerBot[0].body == counter ) 
-        {
-               
-            lastAnswerUser[0] = mensaje._data.body;
-            lastAnswerBot[0].body = lastAnswerBot[1].body
-            console.log(lastAnswerBot[0].body, "lastAnswerBot[0].body 0/2")
-                     
-          
-        } 
-        
-
-        if (lastAnswerUser.length === 0 && lastAnswerBot[0].body == menu ) // Si nadie ha hablado y solamente habla el bot - Inicio de conversación
-        {
-            console.log(mensaje, 'Valores de mensaje 0/0')
-                
-            lastAnswerUser[0] = mensaje._data.body;
-            lastAnswerBot[0].body = lastAnswerBot[1].body
-            console.log(lastAnswerBot[0].body, "lastAnswerBot[0].body 0/2")
-                     
-          
-        } 
-
-
-        if(mensaje._data.body == startConversation || mensaje._data.body == 'hola' || mensaje._data.body == 'klk' || mensaje._data.body == 'hola '
-        || mensaje._data.body == 'Hola ' || mensaje._data.body == 'Hola'  || mensaje._data.body == 'HOLA' || mensaje._data.body == 'HOLA ' || mensaje._data.body == 'klk ' || mensaje._data.body == 'Klk' || mensaje._data.body == 'Klk '  )
-        {
-     
-            return  client.sendMessage(msg.from, menu);
-
-   
+    } else {
+        if (
+            lastAnswerBot.length === 0
+        ) {
+            return client.sendMessage(msg.from, menuOptions);
         }
-            
-        if (mensaje._data.body == option1R || mensaje._data.body == '1r' || mensaje._data.body == '1' && lastAnswerBot[0].body == menu  ) // Selección en el menu 
-             {
+        if (
+            messageIncoming.body == 'Hola' ||
+            messageIncoming.body == 'hola' ||
+            messageIncoming.body == 'klk' ||
+            messageIncoming.body == 'hola ' ||
+            messageIncoming.body == 'Hola ' ||
+            messageIncoming.body == 'Hola' ||
+            messageIncoming.body == 'HOLA' ||
+            messageIncoming.body == 'HOLA ' ||
+            messageIncoming.body == 'klk ' ||
+            messageIncoming.body == 'Klk' ||
+            messageIncoming.body == 'Klk '
+        ) {
+            return client.sendMessage(msg.from, menuOptions);
+        }
 
-            return  client.sendMessage(msg.from, ficha);
-            }
+        if (
+            messageIncoming.body == '1R' ||
+            messageIncoming.body == '1r' ||
+            messageIncoming.body == '1' &&
+            lastAnswerBot[0].body == menuOptions
+        ) {
+            // Selección en el menu
+            return client.sendMessage(msg.from, askfichaMessage);
+        }
 
-            
-        if (lastAnswerBot[0].body == ficha || lastAnswerBot[0].body == ficha2 ) // Captura respuesta de la  ficha  usuario
-        {
-            console.log(mensaje._data.body, 'Valor de body en 0/ficha2 ')
-            
+        if (lastAnswerBot[0].body == askfichaMessage ||
+            lastAnswerBot[0].body == fixValidatorFicha
+        ) {
+            // Captura respuesta de la  ficha  usuario
+            regficha = messageIncoming.body;
 
-            regficha = mensaje._data.body;
-            
-             
-            if(/[a-zA-Z]/.test(regficha) == true && regficha.length == 6 && /[-]/.test(regficha) == true && /[0-9]/.test(regficha) == true  ) // validar condición de ficha 2
+            if (
+                (/[a-zA-Z]/).test(regficha) == true &&
+                (/[0-9]/).test(regficha) == true &&
+                 regficha.length == 6 &&
+                (/[-]/).test(regficha) == true
+            )
             {
+                // validar condición de ficha 2
                 regficha.toUpperCase();
                 regficha.trim();
-            
-            }else{
-                console.log(mensaje._data.body, 'Valor de body en 0 ')
-                return  client.sendMessage(msg.from, ficha2);
+                
             }
-                
+            else
+            {
+                console.log(messageIncoming.body, 'Valor de body en 0 ');
+                return client.sendMessage(msg.from, fixValidatorFicha);
+            }
 
-            return  client.sendMessage(msg.from, counter);
+            return client.sendMessage(msg.from, askCounterOdometer);
         }
 
-        
-        if (lastAnswerBot[0].body == counter)  /// Captura de respuesta de Odometro/Horometro de usuario.
-             {
-                regOdometro = mensaje._data.body;
-                if(/[a-zA-Z]/.test(regOdometro))
-                {
-                    return  client.sendMessage(msg.from, counter);
+        if (lastAnswerBot[0].body == askCounterOdometer
+            ||
+            lastAnswerBot[0].body == fixValidatorOdometer
+        )
 
+        {
+            /// Captura de respuesta de Odometro/Horometro de usuario.
+            regOdometro = messageIncoming.body;
+            if ((/[a-zA-Z]/).test(regOdometro)) {
+                return client.sendMessage(msg.from, fixValidatorOdometer);
+            }
+            regOdometro = regOdometro.replace(/[^0-9.]+/g, ''); //Allow number incluid floating number
+            client.sendMessage(msg.from, messageRegDataConfirm);
+
+            let data = {
+                name: messageIncoming._data.notifyName,
+                Ficha: regficha,
+                Numero: numberFrom,
+                Horometro: regOdometro,
+            };
+
+            posdata.postData(urlPowerautomate, data).then((data) => {
+                console.log(data);
+                if (data.ok) {
+                    // Send message en clear chat
+                    client.sendMessage(
+                        msg.from,
+                        '✅ Datos Enviados correctamente a la base de datos.'
+                    );
+                    client.sendMessage(
+                        msg.from,
+                        `👍 *${messageIncoming._data.notifyName}* Se envió la Ficha: *${regficha}* y el Odometro/Horometro: *${regOdometro}*`
+                    );
+                    return client
+                        .sendMessage(
+                            msg.from,
+                            `🛠 Cualquier inconveniente favor de enviarnos un ticket a *soporte@constructorarizek.com* o llamarnos al *_809-372-2680_* EXT: *_231_*.
+                                    ✏ Escribe *Hola* para iniciar el *Bot*`
+                        )
+                        .then(chatIdInfo.clearMessages());
                 }
-                regOdometro = regOdometro.replace(/[^0-9.]+/g, ''); //Allow number incluid floating number
-                client.sendMessage(msg.from, regDataConfirm);
-    
-              
-                            
-                        let data = {
-                            name: mensaje._data.notifyName,
-                            Ficha: regficha,
-                            Numero: numberFrom,
-                            Horometro: regOdometro
-                            
-                        };  
-                
-                            posdata.postData(urlPowerautomate, data).then((data) => {
-                                console.log(data);
-                                if(data.ok)
-                                { 
-                                    // Send message en clear chat
-                                    client.sendMessage(msg.from,`✅ Datos Enviados correctamente a la base de datos.`);
-                                    client.sendMessage(msg.from,`👍 *${mensaje._data.notifyName}* Se envió la Ficha: *${regficha}* y el Odometro/Horometro: *${regOdometro}*`); 
-                                    return client.sendMessage(msg.from,`🛠 Cualquier inconveniente favor de enviarnos un ticket a *soporte@constructorarizek.com* o llamarnos al *_809-372-2680_* EXT: *_231_*.
-                                    ✏ Escribe *Hola* para iniciar el *Bot*`).then(chatIdInfo.clearMessages());
-                                   
-                                             
-
-                                                
-                                } 
-                            });
-
-              
-           
-
-
-                          
+            });
         }
+
+   
+
+
     }
 });
 
-
-client.on("change_state", (state) => {
-    console.log("CHANGE STATE", state);
+client.on('change_state', (state) => {
+    console.log('CHANGE STATE', state);
 });
 
-client.on("disconnected", (reason) => {
-    console.log("Client was logged out", reason);
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
 });
