b343203d89374c66a2ad1ec8e9e9739d94bb72ca
unknown <luishumeau90@gmail.com>
2023-03-13 15:12:07 -0400

    Create List and projects

--- utils/createList.js
+++ utils/createList.js
@@ -2,17 +2,11 @@ const { List } = require('../index');
 
 
 function ListFactory(options) {
-    const { title, rows, description, footerText } = options;
+    const { title, description,rows, footerText } = options;
     return new List(title, description, rows, footerText);
 }
 
 
-var VehicleListType = new List(
-    'Lista de vehiculos livianos',
-    'Ver todas las fichas',
-    vehicleListJson,
-    'Por favor seleccione una ficha'
-);
 
 
 module.exports = {
